6ae692db258a24310474c357e85ca841d557b725
moreofgeet25 <93831487+moreofgeet25@users.noreply.github.com>
2022-10-28 12:10:53 +0530

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        // code 
+        
+        vector<vector<bool>> vis(n , vector<bool>(m , 0));
+        queue<pair<int,int>> q;
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                    if(mat[i][j] == 'O'){
+                        if(i == 0 || j == 0 || i == n- 1|| j == m -1){
+                            q.push({i,j});
+                            vis[i][j] = 1;
+                        }
+                    }
+            }
+        }
+        
+        int dx[] = { -1, +1, 0, 0}; // up down left right
+        int dy[] = {0, 0, +1, -1};
+
+	    while(!q.empty()){
+	        
+	        int row = q.front().first;
+	        int col = q.front().second;
+	        q.pop();
+	             
+	             for(int k = 0 ; k < 4 ; k++){
+	                 int i = row + dx[k]; int j = col + dy[k]; 
+	                 
+	                 if(i >= 0 && i < n && j >=0  && j < m && !vis[i][j]
+	                 && mat[i][j] == 'O'){
+	                     q.push({i,j});
+	                     vis[i][j] = 1;
+	                 }
+	             }
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int  j = 0 ;  j < m ; j++){
+                
+                if(mat[i][j] == 'O' && !vis[i][j]){
+                    mat[i][j] = 'X';
+                }
+                
+            }
+        }
+        return mat;
+    }
+};
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
