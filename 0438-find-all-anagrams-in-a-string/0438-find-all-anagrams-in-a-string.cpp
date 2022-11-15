b12282cd66fd275498fb4cf6f055537357d5e8e2
moreofgeet25 <93831487+moreofgeet25@users.noreply.github.com>
2022-11-15 17:06:00 +0530

    Time: 17 ms (86.46%), Space: 8.7 MB (40.08%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        
-        vector<int> temp1(26 , 0) , temp2(26 , 0);
-        for(int i = 0 ;i <  p.size() ; i++){
+        int i = 0 , j = 0 , n = s.size() , m = p.size();
+        vector<int> temp1(26 , 0) , temp2(26 , 0) , ans;
+        for(int i = 0 ;i < m ; i++)
             temp1[p[i] - 'a']++;
-        }
-        
-        int i = 0 , j = 0 , n = s.size() , m = p.size() , cnt = 0;
-        vector<int> ans;
-        
         while(j < n){
             temp2[s[j] - 'a']++;
             if(j - i + 1 < m){
                 j++;
-            }else if(j - i + 1 == m){
-                  
-                bool flag = 1;
-                for(int i = 0 ;i < 26 ; i++){
-                    if(temp1[i] != temp2[i]){
-                        flag = 0;
-                    }
-                }
-                if(flag) {
+            }else if(j - i + 1 == m){ 
+                if(temp1 == temp2) {
                     ans.push_back(i);
-                    // cnt++;
-                }
-                
+                }   
                 // slide the window
                 temp2[s[i] - 'a']--;
                 i++;
                 j++;
-            }
+            }   
         }
-        // cout<<cnt<<endl;
         return ans;
     }
 };
